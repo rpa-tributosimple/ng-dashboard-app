8cf9debe75d7e1df89142559532980dc9278ed3c
ernesto <rpa@tributosimple.com>
2025-05-26 08:23:59 -0500

    CREACION DE SECTION Y GRAFICO INTERACTIVO

---
+++ src/app/components/chart/chart.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  standalone: true,
+  selector: 'app-chart',
+  imports: [CommonModule, HttpClientModule, NgxChartsModule, ButtonModule],
+  templateUrl: 'chart.component.html',
+  styleUrls: ['chart.component.css']
+})
+export class ChartComponent implements OnInit {
+
+  chartData: any[] = [];
+  colorScheme = 'vivid';
+
+  monthOffset = 0;
+  currentMonthName = '';
+  monthTitle: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadChartData();
+  }
+
+  loadChartData() {
+    this.http.get<any>(`http://localhost:8000/chart/summary?month_offset=${this.monthOffset}`)
+      .subscribe(response => {
+        const data = response.individuals_ready_tasks;
+        this.monthTitle = response.month
+        this.chartData = Object.entries(data)
+          .filter(([key]) => key !== 'totals')
+          .map(([name, value]) => ({ name: name.toUpperCase(), value }));
+
+        this.updateMonthName();
+      });
+  }
+
+  cambiarMes(offset: number) {
+    this.monthOffset += offset;
+    this.loadChartData();
+  }
+
+  updateMonthName() {
+    const targetDate = new Date();
+    targetDate.setMonth(targetDate.getMonth() + this.monthOffset);
+
+    const monthNames = [
+      'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    ];
+
+    this.currentMonthName = `${monthNames[targetDate.getMonth()]} ${targetDate.getFullYear()}`;
+  }
+}
